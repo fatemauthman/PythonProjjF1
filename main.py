b9d3ba1f8415da0b926f7e2e65b8580b900528e9
fatemauthman <fatemaothman768@gmail.com>
2026-01-30 23:58:09 +0200

    deployment

--- main.py
+++ main.py
@@ -92,6 +92,7 @@ local_time = datetime.now().strftime("%A, %d %B %Y, %H:%M")
 
 
 
+
 st.subheader("Date & Time")
 
 st.write(f"**Your local time:** {local_time}")
--- weather_logic.py
+++ weather_logic.py
@@ -4,22 +4,7 @@ from datetime import datetime, timezone, timedelta
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 def get_weather(city, api_key, units="metric"):
-    params = {
-        "q": city,
-        "appid": api_key,
-        "units": units
-    }
-    response = requests.get(BASE_URL, params=params)
-    response.raise_for_status()
-    data = response.json()
-
-    return {
-        "city": data["name"],
-        "temperature": data["main"]["temp"],
-        "humidity": data["main"]["humidity"],
-        "condition": data["weather"][0]["description"],
-        "timezone": data["timezone"]
-    }
+    ...
 
 def get_location_time(timezone_offset):
     utc_time = datetime.now(timezone.utc)
@@ -28,17 +13,8 @@ def get_location_time(timezone_offset):
 
 from datetime import datetime, timezone, timedelta
 
-def get_location_time(timezone_offset):
-    """
-    timezone_offset: offset in seconds from UTC (OpenWeatherMap)
-    """
-    utc_now = datetime.now(timezone.utc)
-    location_time = utc_now + timedelta(seconds=timezone_offset)
-    return location_time.strftime("%A, %d %B %Y, %H:%M")
-return {
-    "city": data["name"],
-    "temperature": data["main"]["temp"],
-    "humidity": data["main"]["humidity"],
-    "condition": data["weather"][0]["description"],
-    "timezone": data["timezone"]
-}
+
+            def get_location_time(timezone_offset):
+                """
+                timezone_offset: offset in seconds from UTC (from OpenWeatherMap)
+                """
